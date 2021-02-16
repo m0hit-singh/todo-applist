cbe35658ed22d5674716a8b7979a9b568ab3b6eb
mohit singh <mohit.singh@polestarllp.com>
2021-02-16 20:08:00 +0530

    Updates

---
+++ precache-manifest.2e4289ff9c50fb1606fe874c248f19c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1fd3c775a1c5a20691079550210b50",
+    "url": "/todo-applist/index.html"
+  },
+  {
+    "revision": "7670fc11942121e23a5a",
+    "url": "/todo-applist/static/css/main.ea165a26.chunk.css"
+  },
+  {
+    "revision": "7f988be4c17316106a23",
+    "url": "/todo-applist/static/js/2.74d0bef5.chunk.js"
+  },
+  {
+    "revision": "5f983ef0d998e4957796cf6a040e4dc4",
+    "url": "/todo-applist/static/js/2.74d0bef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458a06f77dc9c58a8cc9",
+    "url": "/todo-applist/static/js/3.f0f78947.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/todo-applist/static/js/3.f0f78947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5bacb354aaf05365ee",
+    "url": "/todo-applist/static/js/4.e065640d.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/todo-applist/static/js/4.e065640d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4477b522dfdd1225439b",
+    "url": "/todo-applist/static/js/5.283e69b1.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/todo-applist/static/js/5.283e69b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7670fc11942121e23a5a",
+    "url": "/todo-applist/static/js/main.3e34bd48.chunk.js"
+  },
+  {
+    "revision": "9323a11b758caa089970",
+    "url": "/todo-applist/static/js/runtime-main.f750c0e0.js"
+  }
+]);
